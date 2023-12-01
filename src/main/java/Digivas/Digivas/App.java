046c4f767ba90ed56ef103e31b713042511944d8
QAMohamedAli <bekkeydali4@gmail.com>
2023-12-01 15:25:51 +0100

    first commit

---
+++ src/main/java/Digivas/Digivas/App.java
@@ -0,0 +1,13 @@
+package Digivas.Digivas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
